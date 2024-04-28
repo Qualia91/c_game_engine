d18e71ed61a89699218e851196b51ffa3774f066
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2024-04-28 13:21:05 +0100

    Windows Platform Level

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,5 +1,6 @@
 #include "logger.h"
 #include "asserts.h"
+#include "platform/platform.h"
 
 // TODO: TEMP
 #include <stdio.h>
@@ -21,20 +22,25 @@ void shutdown_logging() {
 
 void log_output(log_level level, const char* message, ...) {
     const char* level_strings[6] = {"[FATAL]:   ", "[ERROR]:   ", "[WARNING]: ", "[INFO]:    ", "[DEBUG]:   ", "[TRACE]:   "};
-    // b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARNING;
 
     // Do this to create message on stack and not dynamically
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, 32000, message, arg_ptr);
+    vsnprintf(out_message, msg_length, message, arg_ptr);
     va_end(arg_ptr);
 
-    char final_out_message[32000];
+    char final_out_message[msg_length];
     memset(final_out_message, 0, sizeof(final_out_message));
     sprintf(final_out_message, "%s%s\n", level_strings[level], out_message);
 
-    printf("%s", final_out_message);
+    if (is_error) {
+        platform_console_write_error(final_out_message, level);
+    } else {
+        platform_console_write(final_out_message, level);
+    }
 }
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// test
+#include <platform/platform.h>
+
 int main(void) {
     KFATAL("A test: %f", 3.14f);
     KERROR("A test: %f", 3.14f);
@@ -9,7 +12,14 @@ int main(void) {
     KDEBUG("A test: %f", 3.14f);
     KTRACE("A test: %f", 3.14f);
 
-    KASSERT(1 == 0);
+    platform_state state;
+
+    if (platform_startup(&state, "Test window", 100, 100, 1280, 720)) {
+        while (TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     return 0;
 }
